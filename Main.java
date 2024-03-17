d171b063726d43e756b9bd979ec3a59e2d3a8e21
KRISHAN1027470 <163717177+KRISHAN1027470@users.noreply.github.com>
2024-03-17 14:21:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+class main{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("0769696491");
+        System.out.println("BCAS Campus");
+        System.out.println("Colombo");
+        System.out.println("Krishan");
+        System.out.println("Batch No 85");
+        System.out.println("ICT");
+        System.out.println("I Love Java");
+        System.out.println("Kollupitiya");
+        System.out.println("Class No F03");  
+    }
+}
